59c8e63de17a85fd177cf54bffb558e71c4fef7e
Andreas Loupasakis <alup@aloop.org>
2017-07-25 22:56:30 +0300

    Add skeleton for python packaging

---
+++ README.rst
@@ -0,0 +1,10 @@
+Greek Stemmer
+-------------
+
+This is a Python implementation of Skroutz's Ruby Greek stemmer.
+
+To use simply do:
+
+    >>> from greek_stemmer import GreekStemmer
+    >>> stemmer = GreekStemmer()
+    >>> stemmer.stem('ΘΑΛΑΣΣΑ')
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from os import path
+from codecs import open
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='greek_stemmer',
+    version='0.1.0',
+    description='Python Greek stemmer.',
+    long_description=long_description,
+    author='Andreas Loupasakis',
+    author_email='alup@skroutz.gr',
+    url='https://github.com/alup/python_greek_stemmer',
+    py_modules=["greek_stemmer"],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=['PyYAML'],
+    license='MIT',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    keywords='stemmer greek nlp',
+    extras_require={
+        'test': ['pytest'],
+    },
+)
