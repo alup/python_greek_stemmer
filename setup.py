8b2c1e2ba9e64610c62c647331eea7c8587d203a
Andreas Loupasakis <alup@aloop.org>
2017-09-11 23:23:54 +0300

    Specify support of Python 3 in the list of classifiers of setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
     ),
     keywords='stemmer greek nlp',
     extras_require={
